c34328293a634e594971730f599ac0ba8427c42e
Martin <Martin.neumayer@stud.fh-rosenheim.de>
2016-03-17 13:22:49 +0100

    Initial Commit, Projekt erstellt

---
+++ CordovaStarter/CordovaStarter/scripts/index.ts
@@ -0,0 +1,37 @@
+﻿// Eine Einführung zur leeren Vorlage finden Sie in der folgenden Dokumentation:
+// http://go.microsoft.com/fwlink/?LinkID=397705
+// So debuggen Sie Code beim Seitenladen in Ripple oder auf Android-Geräten/-Emulatoren: Starten Sie die App, legen Sie Haltepunkte fest, 
+// und führen Sie dann "window.location.reload()" in der JavaScript-Konsole aus.
+module CordovaStarter {
+    "use strict";
+
+    export module Application {
+        export function initialize() {
+            document.addEventListener('deviceready', onDeviceReady, false);
+        }
+
+        function onDeviceReady() {
+            // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
+            document.addEventListener('pause', onPause, false);
+            document.addEventListener('resume', onResume, false);
+
+            // TODO: Cordova wurde geladen. Führen Sie hier eine Initialisierung aus, die Cordova erfordert.
+            var element = document.getElementById("deviceready");
+            element.innerHTML = 'Device Ready';
+            element.className += ' ready';
+        }
+
+        function onPause() {
+            // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
+        }
+
+        function onResume() {
+            // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
+        }
+
+    }
+
+    window.onload = function () {
+        Application.initialize();
+    }
+}
