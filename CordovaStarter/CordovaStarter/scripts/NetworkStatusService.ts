90a729c5212c42486c6ea0f85cd2451cdced9697
Martin <Martin.neumayer@stud.fh-rosenheim.de>
2016-03-17 16:38:24 +0100

    NetworkStatusService implementiert

---
+++ CordovaStarter/CordovaStarter/scripts/NetworkStatusService.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path="../scripts/typings/cordova/plugins/NetworkInformation.d.ts" />
+class NetworkStatusService implements INetworkStatusService {
+    deviceIsOnline: boolean;
+
+    constructor() {
+        document.addEventListener("deviceready", () => {
+            if (navigator.connection.type === Connection.NONE) {
+                this.deviceIsOnline = false;
+            } else {
+                this.deviceIsOnline = true;
+            }
+        });
+
+        document.addEventListener("offline", () => {
+            this.deviceIsOnline = false;
+        });
+        document.addEventListener("online", () => {
+            this.deviceIsOnline = true;
+        });
+    }
+}
